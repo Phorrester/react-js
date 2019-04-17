8a75c73f3ea89c0c25d1e289122a337bcf0c1f39
Cody <48297758+Phorrester@users.noreply.github.com>
2019-04-17 18:16:39 -0300

    Update App.js
    Added Title class that displays name

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Title extends Component{
+  constructor(props){
+    super(props);
+    this.name = props.name;
+  }
+  render(){
+    return (
+      <h1>Hello {name}</h1>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Title name="Cody" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
