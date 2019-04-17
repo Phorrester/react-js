920fbe47e9f91aca8311c8c2487b83997d76186e
Cody <48297758+Phorrester@users.noreply.github.com>
2019-04-17 19:46:38 -0300

    React Project Start
    This is the starting point of my React project.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+const COLORS = ["red", "blue"];
 
-class Title extends Component{
-  constructor(props){
+class Welcome extends Component {
+  constructor(props) {
     super(props);
     this.name = props.name;
   }
-  render(){
+  render() {
     return (
-      <h1>Hello {name}</h1>
+      <h1>Hello {this.name}</h1>
+    )
+  }
+}
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: this.props.color };
+
+  }
+  changeColor = (newColor) => {
+    this.setState({
+      color: newColor
+    });
+  }
+  componentDidMount() {
+
+
+    this.timerID = setInterval(
+      () => {
+        if (this.state.color === "red") {
+          this.changeColor("blue")
+
+        } else {
+          this.changeColor("red")
+
+        }
+      },
+      2000);
+
+
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    return (
+      <div className={this.state.color}></div>
+    )
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return (
+      <div>It is {this.state.date.toLocaleTimeString()}.</div>
+    )
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+}
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.message = props.message;
+  }
+  render() {
+    return (
+      <p>{this.props.message}</p>
     )
   }
 }
@@ -19,21 +96,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Title name="Cody" />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Welcome name="Cody" />
+        <Clock />
+        <Message message="This entire page is made using React." />
+        <Message message="These two squares each have their own state that will cycle between the two colors." />
+        <div className = "inline">
+          <Square color="red" />
+          <Square color="blue" />
+        </div>
       </div>
     );
   }
